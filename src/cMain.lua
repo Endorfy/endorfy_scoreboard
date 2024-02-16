0921e51067c607ffd73a601ddd1c913d1e2b8b7f
fineeasz <80392085+fineeasz@users.noreply.github.com>
2024-02-15 16:28:45 -0800

    optimized

--- src/cMain.lua
+++ src/cMain.lua
@@ -4,20 +4,26 @@ local cache = {}
 local myInfo = {}
 local library = {
     globals = {},
-    scoreboardLastTime = 6000
+    scoreboardLastTime = 6000,
+    scoreboardUses = 5
 }
 
 library.mainThread = function(status)
     library.globals.scoreboardStatus = status
     if library.globals.inUse then return end
     library.globals.inUse = true
-    TriggerServerEvent('endorfy_scoreobard:getInfos')
+    
+    if library.scoreboardUses >= 3 then
+        TriggerServerEvent('endorfy_scoreobard:getInfos')
+        library.scoreboardUses = 0
+    end
 
     if Config.Delay.Enabled and (GetGameTimer() - library.scoreboardLastTime < Config.Delay.Time) then
         return Config.Notify(Config.Locales.waitsometime)
     end
 
     library.scoreboardLastTime = GetGameTimer()
+    library.scoreboardUses = library.scoreboardUses + 1
 
     if status then 
         library.drawThread()
